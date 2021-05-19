8b76c66ac8c4ccab5b75d12de58582ceff73bb36
lyphys <lyphys1@163.com>
2021-05-19 11:59:49 +0800

    完善

--- test.ts
+++ test.ts
@@ -1,15 +1,14 @@
 // tests go here; this will not be compiled when this package is used as an extension.
 
-
 basic.forever(function () {
-    RobotControl.doArmAutoHome()
+    RobotControl.autoHome()
     basic.pause(1000)
-    RobotControl.doArmMoveXYZ(0.0, 0.0, 0.0)
+    RobotControl.moveXYZ(0, 0, 0)
     basic.pause(5000)
-    RobotControl.doArmMoveXYZ(50.0, 50.0, 60.0)
+    RobotControl.moveXYZ(50, 50, 60)
     basic.pause(5000)
-    RobotControl.doArmMoveXYZ(100.0, 100.0, 30.0)
+    RobotControl.moveXYZ(100, 100, 30)
     basic.pause(5000)
-    RobotControl.doArmMoveXYZ(200.0, 200.0, 90.0)
+    RobotControl.moveXYZ(200, 200, 90)
     basic.pause(5000)
 })
